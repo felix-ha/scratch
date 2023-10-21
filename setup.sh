bbe8bc7e9b46ca6b5fce8a53227b53c44beda649
felix-ha <hauerf98@gmail.com>
2023-10-21 16:15:28 +0200

    downgrade torch and pip install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y make docker.io
+sudo apt update && sudo apt install -y make python3-pip
+
+sudo python3 -m pip install --upgrade pip
+sudo pip install -r requirements-dev.txt
+
