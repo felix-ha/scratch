24ce30a97fba04d23cf3c7a6c650daa6c9407a7b
felix-ha <hauerf98@gmail.com>
2023-10-23 21:51:07 +0200

    Use device

---
+++ huggingface/README.md
@@ -0,0 +1,5 @@
+
+fuyu is not possible atm:
+```
+ImportError: cannot import name 'FuyuProcessor' from 'transformers' (/home/paperspace/scratch/huggingface/.venv/lib/python3.9/site-packages/transformers/__init__.py)
+```
--- huggingface/models/mistral-7b.py
+++ huggingface/models/mistral-7b.py
@@ -1,6 +1,10 @@
 from transformers import pipeline
+import torch
 
-pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"{device=}")
+
+pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1", device=device)
 
 print(pipe("I like to eat pizza because it is"))
 
