915726515720e345ca704316a848719697be4c5a
felix-ha <hauerf98@gmail.com>
2023-10-26 22:48:37 +0200

    Improve client

--- huggingface/client.py
+++ huggingface/client.py
@@ -1,19 +1,22 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:5000/model'
-data = {'prompt': 'Mach folgendes: Tue', 'info': 0.5}
+context = ""
+question = ""
+qa_prompt = f"Gegeben diesem Kontext: {context} \n Beantworte die Frage: {question}"
+
+url = 'http://:5000/model'
+data = {'prompt': qa_prompt, 'info': 0.5}
 headers = {'Content-type': 'application/json'}
 
 response = requests.post(url, data=json.dumps(data), headers=headers)
 
 if response.status_code == 200:
     response_data = response.json()
-    prompt = response_data['prompt']
+    result = response_data['result']
     info = response_data['info']
+    print(result[0]['generated_text'])
 
-    print(response.text)
-    print(f'prompt: {prompt}')
-    print(f'Info: {info}')
+
 else:
     print(f'Error: {response.status_code}')
